c330e76ae9547d3ce8958b7e187b7ee37a6cb9f9
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-09 20:30:27 -0500

    start it up

---
+++ src/server/app.js
@@ -0,0 +1,53 @@
+var logger = require('winston'),
+express = require('express'),
+app = express(),
+http = require('http').Server(app),
+//io = require('socket.io')(http),
+//browserify = require('browserify-middleware'),
+bodyParser = require('body-parser');
+
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, { colorize: true });
+
+try {
+  var dotenv = require('dotenv');
+  dotenv.load();
+} catch (e) {}
+
+//app.use('/js/bundle.js', browserify(__dirname+'/../client/app.js'));
+app.use(express.static(__dirname + '/../../public'));
+
+if (process.env.NODE_ENV === "development") {
+  logger.info('development mode');
+
+  app.use(function (req, res, next) {
+    logger.info(req.method + " " + req.path);
+    next();
+  });
+
+  global.debug = function (obj) {
+    var beautify = require('js-beautify').js_beautify;
+    output = beautify(JSON.stringify(obj), { indent_size: 2});
+    logger.info(output);
+  };
+}
+
+// Cross Domain
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Expose-Headers", "X-Filename");
+  res.header("Access-Control-Allow-Headers", "Referer, Range, Accept-Encoding, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+};
+
+app.use(allowCrossDomain);
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(require('./router'));
+
+//var mongoose = require('mongoose');
+//var mongodb_uri = process.env.MONGODB_URI || "mongodb://localhost/mib";
+//mongoose.connect(mongodb_uri);
+//var db = mongoose.connection;
+//db.on('error', logger.error.bind(logger, 'connection error '+mongodb_uri));
+module.exports = http;
---
+++ src/server/router.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var r = module.exports = express.Router();
+//var _ = require('lodash');
+var logger = require('winston')
+  , log = logger.info
+//var Promise = require('bluebird');
+
+var user = process.env.DEWDROP_USER;
+var pass = process.env.DEWDROP_PASS;
+
+if (!user) logger.warn("no DEWDROP_USER set")
+if (!pass) logger.warn("no DEWDROP_PASS set")
+if (!user && !pass) logger.warn('everyone is authorized!')
+else logger.warn("hey! make sure you're using SSL!")
+
+function authorize(req, res, next) {
+  var ok = req.body.username === user && req.body.password === pass
+  if (ok) next()
+  else res.status(401).end()
+}
+
+function act(req, res, next) {
+  actions[req.query.action](req, res, next)
+}
+
+/*
+ * ?action=verify POST username=[YOUR USERNAME] password=[YOUR PASSWORD] // returns JSON, you can check data.success
+ */
+var actions = {
+  verify: function (req, res, next) {
+    next()
+  }
+}
+
+r.route('/').post(authorize, act)
+
+
+/*
+ * ?action=upload POST username=[USERNAME] password=[PASSWORD] filenames=FILENAME file file should be called "userfile" // returns data.success for whether it succeeded or not, and data.message for the URL or error message
+ *
+ * ?action=browse POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false, files as an array of: filename, extension, size
+ *
+ * ?action=get-last-uploaded-file POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false, file with properties: filename, extension, size
+ *
+ * ?action=thumb POST filename=[THE FILENAME YOU WANT A THUMB FOR] // returns a graphical representation - use it as an 
+ *
+ * ?action=remove&filename=[FILENAME] POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false
+ */
