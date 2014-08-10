3ab8211dded0664bfe1ce089866afbaef71aa007
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-09 20:40:58 -0500

    stub the rest, add readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 dewdrop-server-node
 ===================
+
+yo man i got tired of hitting the cloudapp limit so i helped a php brother out by rewritin' his server in node yo
--- src/server/router.js
+++ src/server/router.js
@@ -4,24 +4,6 @@ var r = module.exports = express.Router();
 var logger = require('winston')
 //var Promise = require('bluebird');
 
-var user = process.env.DEWDROP_USER;
-var pass = process.env.DEWDROP_PASS;
-
-if (!user) logger.warn("no DEWDROP_USER set")
-if (!pass) logger.warn("no DEWDROP_PASS set")
-if (!user && !pass) logger.warn('everyone is authorized!')
-else logger.warn("hey! make sure you're using SSL!")
-
-function authorize(req, res, next) {
-  var ok = req.body.username === user && req.body.password === pass
-  if (ok) next()
-  else res.status(401).end()
-}
-
-function act(req, res, next) {
-  actions[req.query.action](req, res, next)
-}
-
 var actions = {
   /*
    * ?action=verify POST username=[YOUR USERNAME] password=[YOUR PASSWORD] // returns JSON, you can check data.success
@@ -34,18 +16,45 @@ var actions = {
    */
   upload: function (req, res, next) {
     res.status(501).end()
+  },
+  /*
+   * ?action=browse POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false, files as an array of: filename, extension, size
+   */
+  browse: function (req, res, next) {
+    res.status(501).end()
+  },
+  /* ?action=get-last-uploaded-file POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false, file with properties: filename, extension, size
+   */
+  'get-last-uploaded-file': function (req, res, next) {
+    res.status(501).end()
+  },
+  /* ?action=thumb POST filename=[THE FILENAME YOU WANT A THUMB FOR] // returns a graphical representation - use it as an 
+   */
+  thumb: function (req, res, next) {
+    res.status(501).end()
+  },
+  /* ?action=remove&filename=[FILENAME] POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false
+   */
+  remove: function (req, res, next) {
+    res.status(501).end()
   }
 }
 
+
+
+
+var user = process.env.DEWDROP_USER;
+var pass = process.env.DEWDROP_PASS;
+if (!user) logger.warn("no DEWDROP_USER set")
+if (!pass) logger.warn("no DEWDROP_PASS set")
+if (!user && !pass) logger.warn('everyone is authorized!')
+else logger.warn("hey! make sure you're using SSL!")
+function authorize(req, res, next) {
+  var ok = req.body.username === user && req.body.password === pass
+  if (ok) next()
+  else res.status(401).end()
+}
+function act(req, res, next) {
+  actions[req.query.action](req, res, next)
+}
 r.route('/').post(authorize, act)
-
-
-/*
- * ?action=browse POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false, files as an array of: filename, extension, size
- *
- * ?action=get-last-uploaded-file POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false, file with properties: filename, extension, size
- *
- * ?action=thumb POST filename=[THE FILENAME YOU WANT A THUMB FOR] // returns a graphical representation - use it as an 
- *
- * ?action=remove&filename=[FILENAME] POST username=[USERNAME] password=[PASSWORD] // returns data.success for true/false
- */
